ed905f49d95ec0a43b9f850f82f266fd12aa4548
djbeadle <Djbeadle@dyn143204.cc.lehigh.edu>
2014-08-25 10:38:41 -0400

    Original Push

---
+++ nmap_parser.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import os
+os.system("echo 'Beginning Scan...'")
+os.system("sudo nmap -sP 10.0.0.1-20>~/soundoff/nmap_output.txt")
+import re
+from prettytable import PrettyTable
+# This, of course, is the output from Nmap
+nmap = open("/home/pi/soundoff/nmap_output.txt","r")
+# This is where we get the Hostname/MAC address combos
+dictionary_ref = open("/home/pi/soundoff/dictionary_reference.txt", "r")
+ip_addresses = []
+mac_addresses = []
+match_ip_var = "cats"
+mac_and_hostnames = {}
+
+for line in dictionary_ref:
+   mac, hostname = line.strip().split('=')
+   mac_and_hostnames[mac.strip()] = hostname.strip()
+dictionary_ref.close()
+
+for line in nmap:
+
+   match_ip = re.search("10\.0\.0\.(.*)", line)
+   match_mac = re.search("..:..:..:..:..:..", line)
+
+   if match_ip:
+      ip_addresses.insert(0, match_ip.group(0))
+      match_ip_var = match_ip.group(0)
+
+# For some reason the rPi's own MAC address doesn't register.
+# This manually ads it in when it's static ip is detected
+
+   elif match_ip_var == "10.0.0.8":
+      mac_addresses.insert(0, "B8:27:EB:AA:C3:AA")
+
+   if match_mac:
+      mac_addresses.insert(0, match_mac.group(0))
+
+nmap.close()
+
+# The string is used to display the number on one line
+# The integer is used in the for statement below
+
+number_hosts_int = len(mac_addresses)
+number_hosts_str = str(number_hosts_int)
+
+x=PrettyTable(["Hostname", "MAC", "IP"])
+x.align["Hostname", "MAC", "IP"] = "c"
+x.padding_width = 1
+
+for i in xrange(0,number_hosts_int,1):
+   if mac_addresses[i] in mac_and_hostnames:
+      current_hostname = mac_and_hostnames[mac_addresses[i]]
+   else:
+      current_hostname = "UNKNOWN"
+   x.add_row([current_hostname, mac_addresses[i], ip_addresses[i]])
+
+print x
+print "Number of Hosts: " + number_hosts_str
---
+++ readme.txt
@@ -0,0 +1,9 @@
+Soundoff should live in a folder named, “Soundoff” in your home directory. When Soundoff is run by ./soundoff the bash script runs a basic mmap ping scan of the first 30 IP’s on the network and outputs which respond to the file, “nmap_output.txt”. The python script then parses that file and matches the MAC addresses to a predefined list of hostnames. Unrecognized hostnames are marked as, “UNKNOWN”
+
+To make Soundoff work on your system:
+
+1) Change the ip address range in soundoff.sh to the appropriate values
+2) Populate dictionary_reference.txt with your hostnames. The format should be, “00:11:22:33:44:55=Hostname” with no spaces and only one entry per line. 
+3) OPTIONAL: Add a bash alias so you can run Soundoff from a simple command anywhere. My entry looks like: “alias soundoff=~/soundoff/soundoff.sh”
+
+Soundoff requires the python library prettytable which can be found here: https://code.google.com/p/prettytable/
